970014275be1e5c46c148af849a05958bbbd8fcb
Juli Ovechkina <yuberdisheva@gmail.com>
2023-06-16 18:07:24 +0100

    add secret

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
 
-export const githubToken = core.getInput("GITHUB_TOKEN", {required: true})
-
-console.log(githubToken);
+export const githubToken = core.getInput("github-token", {required: true})
 
 const octokit = github.getOctokit(githubToken)
 console.log(octokit);
+
